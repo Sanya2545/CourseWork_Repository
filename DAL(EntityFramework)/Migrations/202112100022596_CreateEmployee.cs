803de60404e2f489ef797eb099a4660b45f64c98
Sanya2545 <sasha2003bug@gmail.com>
2021-12-10 03:13:19 +0200

    EntityFramework connected, database updated

---
+++ DAL(EntityFramework)/Migrations/202112100022596_CreateEmployee.cs
@@ -0,0 +1,28 @@
+﻿namespace DAL_EntityFramework_.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateEmployee : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Login = c.String(),
+                        Password = c.String(),
+                        Email = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Employees");
+        }
+    }
+}
---
+++ DAL(EntityFramework)/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace DAL_EntityFramework_.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DAL_EntityFramework_.DBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DAL_EntityFramework_.DBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
